96c4d4da0812a967914a50fdce32eedc628beecb
Amar <amar.pudasaini07@gmail.com>
2019-05-04 18:24:34 +0545

    correction made to fragments

--- app/src/main/java/fragments/Signup.java
+++ app/src/main/java/fragments/Signup.java
@@ -9,14 +9,13 @@ import android.view.ViewGroup;
 
 import com.e.onlineshoppingstore.R;
 
-
 /**
  * A simple {@link Fragment} subclass.
  */
-public class Login extends Fragment {
+public class Signup extends Fragment {
 
 
-    public Login() {
+    public Signup() {
         // Required empty public constructor
     }
 
@@ -25,7 +24,7 @@ public class Login extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        return inflater.inflate(R.layout.fragment_signup, container, false);
     }
 
 }
