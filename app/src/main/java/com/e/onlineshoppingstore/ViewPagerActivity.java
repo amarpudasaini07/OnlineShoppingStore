9387da791f76ca25037eae308c406a38bcbfd8c9
Amar <amar.pudasaini07@gmail.com>
2019-05-04 18:54:42 +0545

    Login and Signup Page is done with tab layout and Viewpager

---
+++ app/src/main/java/com/e/onlineshoppingstore/ViewPagerActivity.java
@@ -0,0 +1,33 @@
+package com.e.onlineshoppingstore;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import adapter.ViewPagerAdapter;
+import fragments.Login;
+import fragments.Signup;
+
+public class ViewPagerActivity extends AppCompatActivity {
+
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_pager);
+
+        tabLayout = findViewById(R.id.tabId);
+        viewPager = findViewById(R.id.viewPager);
+
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+
+        adapter.addFragment(new Login(), "Login");
+        adapter.addFragment(new Signup(), "Register");
+
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+}
